d5b5e172b258f24f7032012762a22d57e8ecd669
Rafa <32127655+rgion@users.noreply.github.com>
2019-03-15 16:53:08 +0100

    Add files via upload

--- examen 2ªev/gestionarZoo/src/Zoo.java
+++ examen 2ªev/gestionarZoo/src/Zoo.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -69,7 +69,7 @@ public class Zoo {
         Scanner sn = new Scanner(System.in);
         System.out.println("¿Qué desea ver?");
         System.out.println("1) Aves ordenadas");
-        System.out.println("2) Peces ordenados");
+        System.out.println("2) Peces ordenados"); //los peces se deja para futuras versiones
         System.out.println("3) Salir");
         opcion = sn.nextInt();
         switch (opcion) {
